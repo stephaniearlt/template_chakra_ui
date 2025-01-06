f9bb4892ee22320e2d7bce4c646c0a9e3c0463aa
Stéphanie Arlt <contact@stephaniearlt.fr>
2025-01-06 11:38:27 +0100

    Initial commit for template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Demo from "@/components/Demo/Demo";  
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Chakra UI with TypeScript</h1>
+      <Demo /> {/* Affiche le composant Demo */}
+    </div>
+  );
+};
+
+export default App;
